04214a12ade27168500fb6e3c8155c215aac754a
Hitesh Agja <hiteshagja24@gmail.com>
2022-02-15 18:15:54 +0530

    test comment

--- src/libs/Navigation/CustomActions.js
+++ src/libs/Navigation/CustomActions.js
@@ -74,6 +74,7 @@ function getScreenNameFromState(state) {
  */
 function getActiveState() {
     // We use our RootState as the dispatch's state is relative to the active navigator and might not contain our active screen.
+    // test comment
     return navigationRef.current.getRootState();
 }
 
